2d9146ae78e42016a46fc171199364a97c9c649c
Tony Schroeder <tony.schroeder@gmx.de>
2022-08-27 16:07:45 +0200

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MenusComponent } from './menus/menus.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    MenusComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
