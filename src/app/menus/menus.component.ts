bbcc5be4c263d4850b5cbecdbb97a01344652e9b
Tony Schroeder <tony.schroeder@gmx.de>
2022-08-29 18:58:05 +0200

    get no image

--- src/app/menus/menus.component.ts
+++ src/app/menus/menus.component.ts
@@ -10,11 +10,15 @@ export class MenusComponent implements OnInit {
 
 imageFound:boolean = true;
 
+noImage = 'assets/img/fork.jpg';
+
   constructor(public foods: FoodsService) { }
 
   async ngOnInit() {
      await this.foods.funcName();
-    console.log(this.foods.menus);
+    // console.log(this.foods.menus);
+    console.log('image', this.foods.menus[3].food[4].img);
+
   }
 
 }
